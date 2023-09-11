ba3e94f828a943123c89340b181532089e505382
Bayangmbe Mounmo <bayangp0@gmail.com>
2023-09-11 02:34:19 +0200

    attemp to solve iframe issue

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["assets.objkt.media"]
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          }
+        ],
+      },
+    ]
   },
+  
 }
 
 module.exports = nextConfig
